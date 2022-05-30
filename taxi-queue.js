d3d259f6ba81b841b93230344a8b3bdba75b1611
Dikgang29 <dcmokonyama29@gmail.com>
2022-05-30 13:35:48 +0200

    unit testing to the taxi que app

--- taxi-queue.js
+++ taxi-queue.js
@@ -1,27 +1,53 @@
 function TaxiQueue() {
 
 
+	let queueCounter = 0;
+
+	let taxiQueueCounter = 0;
 	function joinQueue() {
 
+		queueCounter++;
+		
 	}
 
 	function leaveQueue() {
 
+		if(queueCounter>0){
+		queueCounter--;
+		}
 	}
 
 	function joinTaxiQueue() {
+
+		taxiQueueCounter++;
 
 	}
 
 	function queueLength() {
 
+		return queueCounter;
 	}
 
 	function taxiQueueLength() {
 
+		return taxiQueueCounter;
 	}
 
 	function taxiDepart(){
+
+		if(taxiQueueCounter>0){
+		if(queueCounter>=12){
+			queueCounter= queueCounter- 12;
+			// if(taxiQueueCounter>0){
+			// taxiQueueCounter--;
+			// }
+			taxiQueueCounter--;
+
+
+		}
+		} 
+		return taxiQueueLength();
+
 
 	}
 
