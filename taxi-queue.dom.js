c437b126214a167e0b79e57c82a2ce5cde3c52b8
Dikgang29 <dcmokonyama29@gmail.com>
2022-05-30 14:24:36 +0200

    first commit after working on the DOM

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -2,6 +2,11 @@
 
 
 // DOM element references
+const joinQueueBtn = document.querySelector('.join_queue');
+const leaveQueueBtn = document.querySelector('.leave_queue');
+const passengersCounter = document.querySelector('.passenger_queue_count');
+ 
+
 
 // create Factory Function instance
 
@@ -9,3 +14,7 @@ const taxiQueue = TaxiQueue();
 
 // DOM events
 
+joinQueueBtn.addEventListener('click', function(){
+    taxiQueue.joinQueue();
+    passengersCounter.innerHTML = taxiQueue.queueLength();
+})
